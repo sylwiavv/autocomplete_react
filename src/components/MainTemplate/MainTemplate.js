12315819ac3384213d3af640397e3667d3e9d23d
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2022-03-07 13:35:03 +0100

    Add possibility for remove to do item

--- src/components/MainTemplate/MainTemplate.js
+++ src/components/MainTemplate/MainTemplate.js
@@ -3,6 +3,7 @@ import { Wrapper } from "./MainTemplate.styles";
 import { Title } from "../Title/Title.styles";
 import ResultList from "../ResultList/ResultList";
 import SelectedList from "../SelectedList/SelectedList";
+import ToDo from "../ToDo/ToDo";
 
 const MainTemplate = () => {
     return (
@@ -10,6 +11,7 @@ const MainTemplate = () => {
             <Title>Autocomplete React</Title>
             <ResultList />
             <SelectedList />
+            <ToDo />
         </Wrapper>
     );
 };
