443e53b14d5e7a6847984d4a90beaa4a46327557
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2022-03-20 15:31:39 +0100

    Add some style variables

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,29 +1,64 @@
 // zmienne globalne, funcke jak w csss, helpery
 
 export const theme = {
-  colors: {
-    white: '#F4F5F9',
-    lightGrey: '#F7F8FA',
-    grey: '#C0C7D6',
-    darkGrey: '#737C8E',
-    black: '#161D28',
-    success: '#8FCB81',
-    error: '#FF8383',
-    warning: '#E1D888',
-    darkPurple: '#C0C7D6',
-    lightPurple: '#eff4fc',
-    lightGreen: '#CFECF4'
-  },
-  fontSize: {
-    xxl: '34px',
-    xl: '24px',
-    l: '17px',
-    m: '12px',
-    s: '11px',
-  },
-  fontWeight: {
-    bold: '500',
-    semibold: '600',
-    bolder: '700'
-  }
+    colors: {
+        backgroundColor: '#2d3d4b',
+        fontColor: '#f3f3ff',
+        listsFontColor: '#f3f3ff',
+
+        resultListBackgroundColor: '#fff',
+        resultListBackgroundColorHover: '#2d3d4b',
+
+        resultListFontColor: '#2d3d4b',
+        resultListFontColorHover: '#fff',
+
+        selectedListItemBackgroundColor: '#2d3d4b',
+
+        inputBackgroundColor: '#fff',
+
+        deleteIconBackgroundColor: '#efefff',
+        deleteIconBackgroundColorHover: '#ffffff',
+
+        borderColor: '#e9e9e9',
+    },
+
+    // font-size: ${({ theme }) => theme.fontSize.s};
+    fontSize: {
+        xxl: '36px',
+        xl: '24px',
+        l: '18px',
+        m: '14px',
+        s: '11px',
+    },
+
+    fontWeight: {
+        bold: '500',
+        semiBold: '600',
+        bolder: '700'
+    },
+
+    borderRadius: {
+        xs: '4px',
+        s: '8px',
+        m: '16px',
+    },
+
+  // padding: ${({ theme }) => theme.indents.s};
+  // margin: ${({ theme }) => theme.indents.s};
+  indents: {
+        xs: '4px',
+        s: '8px',
+        m: '16px',
+        l: '24px',
+        xl: '32px',
+        xxl: '40px',
+    },
+
+    boxShadow: {
+        primary: '0px 10px 70px rgb(0 0 0 / 15%)',
+    },
+
+    height: {
+        input: '56px',
+    }
 };
