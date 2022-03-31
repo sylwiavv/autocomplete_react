299eed517f5395fb284ece76048d5606d82d12a3
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2022-03-31 17:28:15 +0200

    Add local storage to save theme mood

--- src/providers/ThemeContext.js
+++ src/providers/ThemeContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 const Theme = createContext({
     toggleMode: () => {},
@@ -7,12 +7,23 @@ const Theme = createContext({
 const ThemeContext = ({ children }) => {
     const [ lightMode, setLightMode ] = useState(false);
 
+    useEffect(() => {
+        const cartItemsData = JSON.parse(localStorage.getItem('cartItems'))
+
+        if (cartItemsData) {
+            setLightMode(cartItemsData)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('cartItems', JSON.stringify(lightMode))
+    }, [lightMode])
+
     const toggleMode = () => {
         lightMode === "light" ?
             setLightMode("dark") :
             setLightMode("light")
     }
-
 
     return (
             <Theme.Provider value = {{ lightMode, toggleMode }}>
